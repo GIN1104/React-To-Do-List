0ed43716c996d5902a5ad87534c094adad7b6764
Evgeny <starchenkogin@gmail.com>
2020-09-08 09:38:03 +0300

    React To-Do-App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FormInput from './components/FormInput';
+import List from './components/List';
+import Footer from './components/Footer';
+import {DataProvider} from './components/DataProvider'
+// import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <DataProvider>
+        <div className="App">
+            <h1>To Do List</h1>
+            <FormInput/>
+            <List/>
+            <Footer/>
+        </div>
+        </DataProvider>
+    );
 }
 
 export default App;
